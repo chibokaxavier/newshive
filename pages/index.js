043883712af8954bde98a23b3fb2205373bbaa8d
manlikexavy <chibokaxavier@gmail.com>
2023-07-03 14:21:24 -0700

    HEADER CREATED

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,12 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
    <main>
-    
+
    </main>
   )
 }
+
+
+// 9791470b6ae34ef4ad08a5cc2dfedeac
+// 
+https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=9791470b6ae34ef4ad08a5cc2dfedeac
