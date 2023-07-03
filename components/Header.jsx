73ac5e9755efbce80be5156285ef751c8584bb4b
manlikexavy <chibokaxavier@gmail.com>
2023-07-03 14:27:08 -0700

    NAVBAR IN PROGRESS

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header>
+      <div className="flex justify-between">
+        <div className="flex space-x-3">
+        <p>About</p>
+        <p>Contact</p>
+        <p>Advertisement</p>
+        <p>Privacy</p>
+        <p>Login</p>
+        </div>
+        <div>
 
-export default Header
+        </div>
+      
+      </div>
+      <div></div>
+      <div></div>
+    </header>
+  );
+};
+
+export default Header;
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,12 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import Header from "@/components/Header";
 
 export default function Home() {
   return (
-   <main>
-
-   </main>
-  )
+    <main>
+      <Header />
+    </main>
+  );
 }
 
-
 // 9791470b6ae34ef4ad08a5cc2dfedeac
-// 
-https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=9791470b6ae34ef4ad08a5cc2dfedeac
+// https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=9791470b6ae34ef4ad08a5cc2dfedeac
