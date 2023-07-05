2f8ad9c177b519db3cd4b8acabefb9f5976b0940
manlikexavy <chibokaxavier@gmail.com>
2023-07-05 12:30:06 -0700

    .

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,45 +3,58 @@ import React from "react";
 const Header = () => {
   return (
     <header className="">
-      <div className="w-[1140px] mx-auto">
-        <div className="flex justify-between ">
+      <div className="lg:w-[1140px] mx-auto">
+        <div className="flex lg:flex-row flex-col items-center  justify-between ">
           <div className="flex space-x-3 uppercase">
             <p>About</p>
             <p>Contact</p>
             <p>Advertisement</p>
             <p>Privacy</p>
-            <p>Login</p>
+            <p className="hidden lg:block">Login</p>
           </div>
+          <p className="lg:hidden">Login</p>
           <div>icons</div>
         </div>
-        <div className="text-5xl font-bold py-5">NEWSHIVE</div>
+        <div className="text-5xl font-bold py-5 items-center flex justify-center lg:items-start lg:justify-start">
+          NEWSHIVE
+        </div>
       </div>
 
-      <div className="bg-black h-[50px] flex justify-between text-white px-2 w-">
-        <div className="flex space-x-5 w-[910px] mx-auto">
+      <div className="bg-black h-[50px] flex justify-between text-white px-2 ">
+        <div className=" space-x-5  lg:w-[910px] mx-auto hidden lg:flex ">
           <div className="border-r border-l border-gray-500 px-2 flex items-center">
             HOME
           </div>
-          <div className=" flex items-center">
-            NEWS
-          </div>
+          <div className=" flex items-center">NEWS</div>
           <div className="border-r border-l border-gray-500 px-2 flex items-center">
             SPORTS
           </div>
-          <div className="flex items-center">
-            LIFESTYLE
-          </div>
+          <div className="flex items-center">LIFESTYLE</div>
           <div className="border-r border-l border-gray-500 px-2 flex items-center">
             FASHION
           </div>
-          <div className="flex items-center">
-            MUSIC
-          </div>
+          <div className="flex items-center">MUSIC</div>
           <div className="border-r border-l border-gray-500 px-2 flex items-center">
             BUISINESS
           </div>
         </div>
-        <div className="flex items-center justify-center bg-gray-900 rounded-lg p-2">
+        <div className="lg:hidden flex justify-center items-center">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M3.75 6.75h16.5M3.75 12h16.5M12 17.25h8.25"
+            />
+          </svg>
+        </div>
+        <div className="flex items-center justify-center  rounded-lg p-2">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -57,7 +70,7 @@ const Header = () => {
             />
           </svg>
 
-          <input type="text" className=" bg-gray-900 outline-none" />
+          <input type="text" className=" bg-transparent pl-2 outline-none" />
         </div>
       </div>
     </header>
